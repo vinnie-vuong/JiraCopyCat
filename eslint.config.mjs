a4a054742071fb2463c0b197468a7a5abfcfc26b
Vicky Vuong <quocviet.vuong2810@gmail.com>
2025-07-05 17:53:35 +0700

    install shadcn and add components, theme

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,13 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "no-unused-var": "error", // <-- This is your new rule
+    },
+  },
+];
 
 export default eslintConfig;
