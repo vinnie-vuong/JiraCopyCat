18f5df78fda1fb9a6fc578b4a49a95d8443a7676
Vicky Vuong <thaiminh.vuong1187@gmail.com>
2025-07-07 17:14:59 +0700

    add sign-in and sign-up page, adjust layout

--- app/layout.js
+++ app/layout.js
@@ -19,13 +19,24 @@ export default function RootLayout({ children }) {
     <ClerkProvider
       appearance={{
         baseTheme: shadesOfPurple,
+        variables: {
+          colorPrimary: "#3b82f6",
+          colorBackground: "#1a202c",
+          colorInputBackground: "#2D3748",
+          colorInputText: "#F3F4F6",
+        },
+        elements: {
+          formButtonPrimary: "text-white",
+        },
       }}
     >
       <html lang="en">
         <body className={`${inter.className}`}>
           <ThemeProvider attribute="class" defaultTheme="dark">
             <Header />
-            <main className="min-h-screen">{children}</main>
+            <main className="min-h-screen">
+              {children}
+            </main>
             <footer className="bg-gray-900 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
                 <p>Made with love by Vinnie 🦄🌈✨👋🌎🌍🌏✨🌈🦄</p>
