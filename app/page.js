7be7fd185420341c16007868606bac576d639d0e
Vicky Vuong <quocviet.vuong2810@gmail.com>
2025-07-08 23:22:09 +0700

    key features in progress

--- app/page.js
+++ app/page.js
@@ -3,6 +3,27 @@ import { ChevronRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
+const features = [
+  {
+    title: "Intuitive Kanban Boards",
+    description:
+      "Visualize your workflow and optimize team productivity with our easy-to-use Kanban boards.",
+    icon: Layout,
+  },
+  {
+    title: "Powerful Sprint Planning",
+    description:
+      "Plan and manage sprints effectively, ensuring your team stays focused on delivering value.",
+    icon: Calendar,
+  },
+  {
+    title: "Comprehensive Reporting",
+    description:
+      "Gain insights into your team's performance with detailed, customizable reports and analytics.",
+    icon: BarChart,
+  },
+];
+
 export default function Home() {
   let hello;
 
@@ -12,7 +33,7 @@ export default function Home() {
       <section className="container mx-auto py-20 text-center">
         <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex flex-col">
           Streamline Your Workflow <br/>
-          <span>
+          <span className="flex mx-auto gap-3 sm:gap-4 items-center">
             with{" "}
             <Image
               src={"/logo2.png"}
@@ -25,8 +46,8 @@ export default function Home() {
         </h1>
         <p>Empower your team with our intuitive project management solution</p>
         <Link href="/onboarding">
-          <Button size="lg" className="mr-4">
-            Get Started <ChevronRight size={18} className="ml-1" />
+          <Button size="lg" className="mr-2">
+            Get Started <ChevronRight size={18}/>
           </Button>
         </Link>
         <Link href="#features">
@@ -34,6 +55,15 @@ export default function Home() {
             Learn More
           </Button>
         </Link>
+      </section>
+
+      <section>
+        <div>
+          <h3>Key Features</h3>
+          <div>
+
+          </div>
+        </div>
       </section>
     </div>
     
