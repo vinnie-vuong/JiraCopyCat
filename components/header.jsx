509a38eb9c5d07cd454664349cdf6f20fb10671b
Vicky Vuong <quocviet.vuong2810@gmail.com>
2025-07-08 08:05:01 +0700

    header component

--- components/header.jsx
+++ components/header.jsx
@@ -1,17 +1,44 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { SignedIn, SignedOut, SignInButton } from '@clerk/nextjs';
 import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { PenBox } from "lucide-react";
+import UserMenu from './user-menu';
 
 const Header = () => {
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
+    <header className="container mx-auto">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={"/logo2.png"}
+            alt="Zscrum Logo"
+            width={200}
+            height={56}
+            className="h-10 w-auto object-contain"
+          />
+        </Link>
+        <div className="flex items-center gap-4">
+          <Link href="/project/create">
+            <Button variant="destructive" className="flex items-center gap-2">
+              <PenBox size={18} />
+              <span>Create Project</span>
+            </Button>
+          </Link>
 
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/onboarding">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserMenu/>
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
   );
 };
 
