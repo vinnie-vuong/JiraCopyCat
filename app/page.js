08d020e24f05e5e38e0470cab9839d49de958dc0
Vicky Vuong <thaiminh.vuong1187@gmail.com>
2025-07-08 17:50:50 +0700

    landing page UI

--- app/page.js
+++ app/page.js
@@ -1,11 +1,41 @@
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   let hello;
 
   return (
-    <div>
-      Subscribe to Vinnie Vuoongnn
+    <div  className="min-h-screen">
+      { /* Hero section */ }
+      <section className="container mx-auto py-20 text-center">
+        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex flex-col">
+          Streamline Your Workflow <br/>
+          <span>
+            with{" "}
+            <Image
+              src={"/logo2.png"}
+              alt="Zscrum Logo"
+              width={400}
+              height={80}
+              className="h-14 sm:h-24 w-auto object-contain"
+            />
+          </span>
+        </h1>
+        <p>Empower your team with our intuitive project management solution</p>
+        <Link href="/onboarding">
+          <Button size="lg" className="mr-4">
+            Get Started <ChevronRight size={18} className="ml-1" />
+          </Button>
+        </Link>
+        <Link href="#features">
+          <Button size="lg" variant="outline" className="mr-4">
+            Learn More
+          </Button>
+        </Link>
+      </section>
     </div>
+    
   );
 }
